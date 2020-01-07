818dca51b7c4082b723b0daf5698a877c2e22941
dumingyue <719128469@qq.com>
2020-01-07 12:33:16 +0800

    解决RadioGroup清除选RadioButton选中状态总是true

--- ESLibraryAndroid/birthstone/src/main/java/com/birthstone/widgets/ESRadioButton.java
+++ ESLibraryAndroid/birthstone/src/main/java/com/birthstone/widgets/ESRadioButton.java
@@ -1,13 +1,37 @@
 package com.birthstone.widgets;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.accessibility.AccessibilityEvent;
 
 public class ESRadioButton extends android.widget.RadioButton
 {
+	private boolean mChecked;
+
+	private OnCheckedChangeListener mOnCheckedChangeListener;
 	public ESRadioButton(Context context, AttributeSet attrs )
 	{
 		super(context, attrs);
 	}
 
+	@Override
+	public void setChecked(boolean checked)
+	{
+		this.mChecked = super.isChecked();
+		if (this.mChecked != checked) {
+			mChecked = checked;
+			refreshDrawableState();
+			if ( mOnCheckedChangeListener!= null) {
+				mOnCheckedChangeListener.onCheckedChanged(this, mChecked);
+			}
+		}
+	}
+
+	@Override
+	public void setOnCheckedChangeListener(@Nullable OnCheckedChangeListener listener)
+	{
+//		super.setOnCheckedChangeListener(listener);
+		mOnCheckedChangeListener = listener;
+	}
 }
